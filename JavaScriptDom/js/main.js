5fdedf704ce4332e984ad8813ffeebc7a9de0994
Jahirul Islam <jahirulislamjantu002@gmail.com>
2023-12-28 10:29:14 +0600

    innerText and innerHTML added

--- JavaScriptDom/js/main.js
+++ JavaScriptDom/js/main.js
@@ -1,7 +1,13 @@
 // JavaScript DOM: finding an element
 
-const tagName = document.getElementsByTagName("h2"); //ট্যাগ নেম দিয়ে যখন এলিমেন্ট ধরবো তখন মাল্টিপল এলিমেন্ট সিলেক্ট হয়ে এর‍্যে আকারে আসবে।
-const className = document.getElementsByClassName("language"); // একই নামের মাল্টিপল ক্লাস থাকতে পারে, তাই এটা এর‍্যে আকারে আসবে।
-const idName = document.getElementById("country"); // আইডি সবসময় ইউনিক হয়, তাই আইডি দিয়ে যখন একটা এলিমেন্ট সিলেক্ট করবো তখন সিংগেল এলিমেন্ট আসবে।
-const qSelector = document.querySelector(); // সিংগেল আইডি বা ক্লাস আকারে সিলেক্ট করার জন্য এই সিলেক্টর ব্যাবহার করা যায়।
-const allqSelect = document.querySelectorAll(); // সকল ক্লাস একবারে সিলেক্ট করার জন্য ব্যাবহার করা হয় ।
+let tagSelector = document.getElementsByTagName("h2"); //ট্যাগ নেম দিয়ে যখন এলিমেন্ট ধরবো তখন মাল্টিপল এলিমেন্ট সিলেক্ট হয়ে এর‍্যে আকারে আসবে।
+let classSelector = document.getElementsByClassName("language"); // একই নামের মাল্টিপল ক্লাস থাকতে পারে, তাই এটা এর‍্যে আকারে আসবে।
+let idSelector = document.getElementById("country"); // আইডি সবসময় ইউনিক হয়, তাই আইডি দিয়ে যখন একটা এলিমেন্ট সিলেক্ট করবো তখন সিংগেল এলিমেন্ট আসবে।
+// let qSelector = document.querySelector(); // সিংগেল আইডি বা ক্লাস আকারে সিলেক্ট করার জন্য এই সিলেক্টর ব্যাবহার করা যায়।
+// let allQSelect = document.querySelectorAll(); // সকল ক্লাস একবারে সিলেক্ট করার জন্য ব্যাবহার করা হয় ।
+
+// JavaScript DOM: innerText Manipulation
+idSelector.innerText = "Bangla is my MotherLanguage";
+
+// JavaScript DOM: innerHTML Manipulation
+idSelector.innerHTML = "<button>Bangladesh!</button>";
